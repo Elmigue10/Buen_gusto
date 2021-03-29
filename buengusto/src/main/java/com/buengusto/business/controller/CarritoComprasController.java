14e374ab805d2fc068473a1c176cee39fee86a64
Deiver20 <deivercamiloprieto@gmail.com>
2021-03-28 23:52:48 -0500

    avance carrito

--- buengusto/src/main/java/com/buengusto/business/controller/CarritoComprasController.java
+++ buengusto/src/main/java/com/buengusto/business/controller/CarritoComprasController.java
@@ -9,6 +9,8 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/carritoCompras")
+@CrossOrigin(origins = "*", methods= {RequestMethod.GET,RequestMethod.POST,
+    RequestMethod.PUT, RequestMethod.DELETE})
 public class CarritoComprasController {
 
     @GetMapping
